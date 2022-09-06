7396dc516c599e25ec7940d6e9000176b3301b0c
Scott Sullivan <scott@sullysbrain.com>
2022-09-05 23:08:38 -0400

    Various Jupyter Notebook ndarray experimentation.

--- python-sandbox.py
+++ python-sandbox.py
@@ -1,4 +1,9 @@
 # Python sandbox file for testing code snippets
+import sys
 
-x = "Hello world"
-print(x)
+#Experimentation with Sets
+x1 = {1, 2, 3}
+x2 = {"dog", "books", 2}
+myNum = 3
+print(f"X1:{x1.union(x2)}")
+
