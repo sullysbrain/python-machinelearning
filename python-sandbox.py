884cfe62ab200c0c386db4c25477333262401466
Scott Sullivan <scott@sullysbrain.com>
2022-09-09 09:10:33 -0400

    Added Jupyter Notebook for PyTorch exploration.

--- python-sandbox.py
+++ python-sandbox.py
@@ -1,9 +1,12 @@
 # Python sandbox file for testing code snippets
 import sys
 
+
 #Experimentation with Sets
 x1 = {1, 2, 3}
 x2 = {"dog", "books", 2}
 myNum = 3
 print(f"X1:{x1.union(x2)}")
 
+x3 = x1.union(x2)
+print(f'x3 is:  {x3}')
