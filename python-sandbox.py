8ffeb115abccccf0f14e182d933aec1c9e7c6eb4
Scott Sullivan <scott@sullysbrain.com>
2022-09-10 18:21:38 -0400

    Added financial import of CSV stock data, added 'Daily Change and 'Normailzed' data do each entry (at the daily level)

--- python-sandbox.py
+++ python-sandbox.py
@@ -1,12 +1,18 @@
 # Python sandbox file for testing code snippets
 import sys
+import logging
 
+logging.basicConfig(encoding='utf-8', level=logging.DEBUG,
+                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__file__)
+
+
+def mymainmethod(x):
+    logger.info(f'root called with {type(x)}')
 
 #Experimentation with Sets
 x1 = {1, 2, 3}
 x2 = {"dog", "books", 2}
-myNum = 3
-print(f"X1:{x1.union(x2)}")
+x = x1.union(x2)
+mymainmethod(x)
 
-x3 = x1.union(x2)
-print(f'x3 is:  {x3}')
